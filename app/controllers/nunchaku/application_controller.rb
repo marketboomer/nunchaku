d01ecc1bd63ab7eb4cd0753285a91acc8778e4fb
Mark Ratjens <mark@ratjens.com>
2014-03-06 13:08:43 +1100

    inventory tests pass

---
+++ app/controllers/nunchaku/application_controller.rb
@@ -0,0 +1,3 @@
+class Nunchaku::ApplicationController < ApplicationController
+
+end
--- app/models/goods/product_translation.rb
+++
@@ -1,7 +0,0 @@
-class Goods::ProductTranslation < ActiveRecord::Base
-  self.table_name = :goods_product_translations
-
-  # Associations
-  belongs_to :product, :class_name => Goods::Product
-
-end
