d18bdef06051b2ab8f0c50671621effbea5dfe8e
Mark Ratjens <mark@ratjens.com>
2014-03-13 10:34:44 +1100

    remove namespace isolation

--- lib/nunchaku/engine.rb
+++ lib/nunchaku/engine.rb
@@ -13,8 +13,6 @@ require 'csv'
 
 module Nunchaku
   class Engine < ::Rails::Engine
-    isolate_namespace Nunchaku
-
     config.generators do |g|
       g.test_framework :rspec, :fixture => false, :view_specs => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
