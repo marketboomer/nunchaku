a0aae3e36623d742055f8769d924706fc5850309
Mark Ratjens <mark@ratjens.com>
2014-04-11 15:30:47 +1000

    straighten out engine view loading precedence to agree with inheritance hierarchy rather than some dumb-ass engine-loading algorithm some pimply gf-less kid dreamt up while playing grand theft auto

--- app/controllers/nunchaku/application_controller.rb
+++ app/controllers/nunchaku/application_controller.rb
@@ -1,3 +1,14 @@
 class Nunchaku::ApplicationController < ApplicationController
 
+  before_filter :settle_engine_view_precedence
+
+  protected
+
+  def settle_engine_view_precedence
+    engine_precedence.reverse.each { |e| prepend_view_path e.root.join('app','views') }
+  end
+
+  def engine_precedence
+    [Nunchaku::Engine]
+  end
 end
