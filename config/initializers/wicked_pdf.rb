f9f0051103b7d19c7f48adf1970bf0d082b7212c
Danny <danny.jankovics@marketboomer.com>
2014-07-23 16:39:13 +1000

    Yet another wickedpdf setup change

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
 WickedPdf.config = {
-    :exe_path => Rails.env.development? ? '/usr/local/bin/wkhtmltopdf' : 'wkhtmltopdf'
+    :exe_path => '/usr/local/bin/wkhtmltopdf'
     # :exe_path => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
 }
