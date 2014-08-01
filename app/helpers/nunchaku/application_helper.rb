f2e2d954a9c04ec367a4d142b8e5d3a74c1cbefb
Mark Ratjens <mark@ratjens.com>
2014-08-01 18:00:03 +1000

    be able to derive an engine path from a resource; use in breadcrumbs

--- app/helpers/nunchaku/application_helper.rb
+++ app/helpers/nunchaku/application_helper.rb
@@ -6,4 +6,13 @@ module Nunchaku::ApplicationHelper
   def engine_name
     controller.class.name.deconstantize.downcase
   end
+
+  def engine_path(*args)
+    l = args.flatten.last
+    klass = l.is_a?(Class) ? l : l.class
+
+    send(path_prefix(klass)).polymorphic_path(args)
+  rescue
+    polymorphic_path(args)
+  end
 end
