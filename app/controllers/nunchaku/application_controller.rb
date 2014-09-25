08d13c5f59fa74c98ca9d00b835fae733bfa6607
Tracy Mu Sung <tracy.musung@marketboomer.com>
2014-09-25 15:20:59 +1000

    Set default response format to html

--- app/controllers/nunchaku/application_controller.rb
+++ app/controllers/nunchaku/application_controller.rb
@@ -1,6 +1,7 @@
 class Nunchaku::ApplicationController < ApplicationController
 
   before_filter :settle_engine_view_precedence
+  before_filter :set_default_response_format
 
   protected
 
@@ -10,5 +11,9 @@ class Nunchaku::ApplicationController < ApplicationController
 
   def engine_precedence
     [Nunchaku::Engine]
+  end
+
+  def set_default_response_format
+    request.format = :html if request.format == "*/*"
   end
 end
