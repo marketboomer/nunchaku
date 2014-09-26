da23eb403436feb42f57affc582ecd957164c6b2
Richard Nichol <richardonichol@gmail.com>
2014-09-26 15:59:12 +1000

    Sort method which now works in both directions.

--- app/controllers/concerns/nunchaku/moving.rb
+++ app/controllers/concerns/nunchaku/moving.rb
@@ -7,7 +7,8 @@ module Nunchaku::Moving
 
   def sort
     ids = params[resource_params_name].map { |id| id.to_i }
-    item = first_moved_item(ids)
+    moved = moved_id(ids)
+    item = resource_class.find(moved)
     item.insert_at(moved_to(ids,item)) # Using acts_as_list
 
     gflash :notice => t("flash.#{resource_class.name.underscore.pluralize.gsub('/', '.')}.sort.notice", { :resource_types =>  human(resource_class).pluralize })
@@ -15,23 +16,30 @@ module Nunchaku::Moving
 
   protected
 
-  def first_moved_item ids
+  def moved_id ids
+    offset = first_changed_index(ids)
+    new_candidate = ids[offset]
+    old_candidate = original_ids[offset]
+    new_delta = (ids.index(new_candidate) - original_ids.index(new_candidate)).abs
+    old_delta = (ids.index(old_candidate) - original_ids.index(old_candidate)).abs
+    new_delta > old_delta ? new_candidate : old_candidate
+  end
+
+  def first_changed_index ids
     ids.each_with_index do |id, i|
-      return resource_class.find(collection_array[i]) if collection_array[i] != id
+      return i if original_ids[i] != id
     end
-    nil
   end
 
   def moved_to ids, item
     ids.index(item.id) + 1
   end
 
+  def original_ids
+    @original_ids ||= resource_class.where(id: params[resource_params_name]).order(:position).pluck(:id)
+  end
+
   def movable_collection
     collection.except(:limit)
   end
-
-  def collection_array
-    @collection_array ||= resource_class.where(:id => params[resource_params_name]).order(:position).pluck(:id)
-  end
-
 end
