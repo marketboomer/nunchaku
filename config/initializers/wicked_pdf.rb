f3fd48c14af1f0168f614e1a3a77f539a58aaaa4
john.song <john1.song@marketboomer.com>
2014-11-26 10:38:09 +1100

    try to use wkhtmltopdf with Qt

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,5 @@
 WickedPdf.config = {
-    :exe_path => '/usr/local/bin/wkhtmltopdf'
+    :exe_path => '/home/ec2user/bin/wkhtmltopdf'
+    # :exe_path => '/usr/local/bin/wkhtmltopdf'
     # :exe_path => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
 }
