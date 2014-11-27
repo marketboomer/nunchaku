f85f50bc8bb500b6be8c8d81299589c7f78bc222
john.song <john1.song@marketboomer.com>
2014-11-28 10:54:43 +1100

    revert wkhtmltopdf path back

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,4 @@
 WickedPdf.config = {
-    :exe_path => '/home/ec2user/bin/wkhtmltopdf'
-    # :exe_path => '/usr/local/bin/wkhtmltopdf'
+    :exe_path => '/usr/local/bin/wkhtmltopdf'
     # :exe_path => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
 }
