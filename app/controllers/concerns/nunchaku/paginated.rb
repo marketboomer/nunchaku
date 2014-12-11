c2db535c5b5fce10e852adcd44fed59cc9991edd
Mark Ratjens <mark@ratjens.com>
2014-12-11 19:42:21 +1100

    use sweet ux count for "More" links in the index table footer

--- app/controllers/concerns/nunchaku/paginated.rb
+++ app/controllers/concerns/nunchaku/paginated.rb
@@ -2,13 +2,13 @@ module Nunchaku::Paginated
   extend ActiveSupport::Concern
 
   included do
-    helper_method :paged_collection
+    helper_method :paged_collection, :current_page, :sweet_ux_count
   end
 
   protected
 
   def collection
-    memoize_collection { paged_formats.include?(request.format.symbol) ? super.page(current_page).per(per_page) : super }
+    memoize_collection { paged_formats.include?(request.format.symbol) ? super.page(current_page).per(per_page).with_read_ahead(read_ahead_count) : super }
   end
 
   def paged_formats
@@ -25,6 +25,19 @@ module Nunchaku::Paginated
 
   def per_page
     params[rows_param_name] || resource_class.default_per_page
+  end
+
+  def read_ahead_count
+    0
+  end
+
+  def sweet_ux_count
+    cc = collection.count
+    cc <= show_ahead_count ? cc : per_page
+  end
+
+  def show_ahead_count
+    per_page + read_ahead_count - 1
   end
 
   def page_param_name
--- app/helpers/nunchaku/resources_page_helper.rb
+++ app/helpers/nunchaku/resources_page_helper.rb
@@ -10,4 +10,12 @@ module Nunchaku::ResourcesPageHelper
   def page_title_type
     (resource? ? human(resource.class) : nested_collection_type).html_safe
   end
+
+  def maybe_more_records
+    if collection.count > sweet_ux_count
+      link_to "#{t(:more_records)}&#8230;".html_safe, params.merge(:page => current_page.to_i + 1), :rel => 'next'
+    else
+      t(:no_more_records)
+    end
+  end
 end
