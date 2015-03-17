501494ca574ce2b27c96f9405be8eb4a26b39e5c
Mark Ratjens <mark@ratjens.com>
2015-03-17 13:02:15 +1100

    refactor cacheable; put union scopes into a concern

---
+++ app/models/concerns/nunchaku/relational.rb
@@ -0,0 +1,17 @@
+module Nunchaku
+  module Relational
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def union_scope(*scopes)
+        id_column = "#{table_name}.id"
+
+        if (sub_query = scopes.reject { |sc| sc.count == 0 }.map { |s| s.select(id_column).to_sql }.join(' UNION ')).present?
+          where "#{id_column} IN (#{sub_query})"
+        else
+          none
+        end
+      end
+    end
+  end
+end
--- app/models/concerns/nunchaku/resourceable.rb
+++ app/models/concerns/nunchaku/resourceable.rb
@@ -2,6 +2,9 @@ module Nunchaku
   module Resourceable
     extend ActiveSupport::Concern
 
+    include Relational
+    include Reflections
     include Pairable
+    include Cacheable
   end
 end
