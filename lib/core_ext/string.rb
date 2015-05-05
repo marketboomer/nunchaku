dd845b47413baca4b678382e9b8cd2f769845526
Danny <danny.jankovics@marketboomer.com>
2015-05-05 17:30:43 +1000

    Add annotate method to string to help with 2 character searching

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,7 +1,11 @@
 #encoding: UTF-8
 class String
   def hanize
-      self.split("").map{|c| c.contains_cjk? ? "#{c}^" : c}.join
+    self.contains_cjk? ? self.annotate : self
+  end
+
+  def annotate(separator = '^')
+    self.split("").join(separator) << separator
   end
 
   def contains_cjk?
