412542cfbdba0b4203c8f3d13b07c63c959e4df9
Tracy Mu Sung <tracy.musung@marketboomer.com>
2015-05-12 11:55:22 +1000

    revert John's change

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
 WickedPdf.config = {
-    #:exe_path => '/usr/local/bin/wkhtmltopdf'
-    :exe_path => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
+    :exe_path => '/usr/local/bin/wkhtmltopdf'
+    # :exe_path => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
 }
