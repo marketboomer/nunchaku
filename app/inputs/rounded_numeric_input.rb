75e3100a8ee6d2dfdcf30b83b4ffba59d7ba362a
Danny <danny.jankovics@marketboomer.com>
2015-06-15 16:53:59 +1000

    #96850818 Don't override value if one is explicitly set

--- app/inputs/rounded_numeric_input.rb
+++ app/inputs/rounded_numeric_input.rb
@@ -1,7 +1,7 @@
 class RoundedNumericInput < ::SimpleForm::Inputs::NumericInput
   def input(*args)
     input_html_classes.unshift('form-control')
-    input_html_options.merge! :value => object.send(attribute_name).to_f.round(2)
+    input_html_options.merge! :value => object.send(attribute_name).to_f.round(2) unless input_html_options.has_key?(:value)
 
     super(*args)
   end
